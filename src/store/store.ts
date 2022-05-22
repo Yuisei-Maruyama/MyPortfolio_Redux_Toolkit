65860c41e98d4f6b2c988338a00e48d6ad3ebae0
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-05-22 22:20:44 +0900

    update: storeにReducerを設定する

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../slice/counterSlice';
 
 // storeの作成
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer,
+  },
 });
