f2e79d41054e3e83b2acaf31602b3661e3abe344
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-05-27 00:16:45 +0900

    add: MongoDBと接続処理追加

---
+++ src/utils/database.ts
@@ -0,0 +1,33 @@
+import { MongoClient, Db } from 'mongodb';
+
+const { MONGODB_URI: uri, MONGODB_DB: dbName } = process.env;
+
+let cachedClient: MongoClient;
+let cachedDb: Db;
+
+if (!uri) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  );
+}
+
+if (!dbName) {
+  throw new Error(
+    'Please define the MONGODB_DB environment variable inside .env.local'
+  );
+}
+
+export const connectToDatabase = async () => {
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb };
+  }
+
+  const client = await MongoClient.connect(uri);
+
+  const db = await client.db(dbName);
+
+  cachedClient = client;
+  cachedDb = db;
+
+  return { client, db };
+};
