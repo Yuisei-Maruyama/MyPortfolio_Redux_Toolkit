cf80521c585176233a2804da1ce4dfed48e94651
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-06-02 00:05:01 +0900

    update: コード整形

--- tools/readme.ts
+++ tools/readme.ts
@@ -1,9 +1,9 @@
-import packageJson from '../package.json';
-import { existsSync, writeFile, unlinkSync } from 'fs';
+import packageJson from '../package.json'
+import { existsSync, writeFile, unlinkSync } from 'fs'
 
 /* eslint  no-multi-str: 0 */
 
-const { dependencies, devDependencies } = packageJson;
+const { dependencies, devDependencies } = packageJson
 
 const readme = ''
 
@@ -27,7 +27,8 @@ const devEnv = ' ### 開発環境について \n\n \
 - MacOS Monterey v12.4 \n\n \
 '
 
-const structure = '### ディレクトリ構成 \n\n \
+const structure =
+  '### ディレクトリ構成 \n\n \
 ```ts \n \
 . \n \
 ├── README.md \n \
@@ -59,80 +60,80 @@ const structure = '### ディレクトリ構成 \n\n \
 ``` \n\n \
 '
 
-const name: string[] = [];
-const version: string[] = [];
+const name: string[] = []
+const version: string[] = []
 
 const getDesc = (packageName: string) => {
   switch (packageName) {
     case '@reduxjs/toolkit':
-      return '@reduxjs/toolkit の説明です';
+      return '@reduxjs/toolkit の説明です'
     case 'next':
-      return 'next の説明です';
+      return 'next の説明です'
     case 'normalizr':
-      return 'normalizr の説明です';
+      return 'normalizr の説明です'
     case 'react':
-      return 'ユーザインタフェース構築のための JavaScript ライブラリ';
+      return 'ユーザインタフェース構築のための JavaScript ライブラリ'
     case 'react-dom':
-      return 'ルーティングを定義するためのライブラリ';
+      return 'ルーティングを定義するためのライブラリ'
     case 'react-redux':
-      return 'react-redux の説明です';
+      return 'react-redux の説明です'
     case 'redux-persist':
-      return 'Storeの状態を永続化するパッケージ';
+      return 'Storeの状態を永続化するパッケージ'
     case '@types/node':
-      return '@types/node の説明です';
+      return '@types/node の説明です'
     case '@types/react':
-      return '@types/react の説明です';
+      return '@types/react の説明です'
     case '@types/react-dom':
-      return '@types/react-dom の説明です';
+      return '@types/react-dom の説明です'
     case '@types/react-redux':
-      return '@types/react-redux の説明です';
+      return '@types/react-redux の説明です'
     case 'eslint':
-      return 'eslint の説明です';
+      return 'eslint の説明です'
     case 'eslint-config-next':
-      return 'eslint-config-next の説明です';
+      return 'eslint-config-next の説明です'
     case 'ts-node':
-      return 'typescript のファイルを単体で実行できるモジュール';
+      return 'typescript のファイルを単体で実行できるモジュール'
     case 'typescript':
-      return 'JavaScript に対して、静的型付けとクラスベースオブジェクト指向を加えた言語ライブラリ';
+      return 'JavaScript に対して、静的型付けとクラスベースオブジェクト指向を加えた言語ライブラリ'
     case 'axios':
       return 'Promise ベースの HTTP Client ライブラリ'
     default:
-      break;
+      break
   }
-  return '';
-};
+  return ''
+}
 
 Object.entries(dependencies).forEach(([key, value]) => {
-  name.push(key);
-  version.push(value);
-});
+  name.push(key)
+  version.push(value)
+})
 
 Object.entries(devDependencies).forEach(([key, value]) => {
-  name.push(key);
-  version.push(value);
-});
+  name.push(key)
+  version.push(value)
+})
 
-let tableBody = '| 技術 | version | 備考 |\n| ---- | ------- | ---- |\n';
+let tableBody = '| 技術 | version | 備考 |\n| ---- | ------- | ---- |\n'
 
 for (let i = 0; i < name.length; i++) {
   tableBody = tableBody.concat(
     `| [${name[i]}](https://www.npmjs.com/package/${name[i]}) | ${version[i]} | ${getDesc(name[i])} |\n`
-  );
+  )
 }
 
 const packageList = `### 使用パッケージ \n\n${tableBody}\n`
 
-const createComKnowledge = '### コンポーネント作成にあたって \n\n \
+const createComKnowledge =
+  '### コンポーネント作成にあたって \n\n \
 VSCode の拡張機能 `Next.js snippets` を導入し、下記を実行する。\n\n \
 ```ts \n \
 nafe; \n \
 ``` \n \
 '
 
-const convertReadme = readme.concat(title, env, devEnv, structure, packageList, createComKnowledge )
-
+const convertReadme = readme.concat(title, env, devEnv, structure, packageList, createComKnowledge)
 
 writeFile('README.md', convertReadme, (err) => {
-  if (err) throw err;
-  console.log('正常に書き込みが完了しました');
-});
+  if (err) throw err
+  console.log('正常に書き込みが完了しました')
+})
