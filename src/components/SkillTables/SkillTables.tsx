22aee912c36c7bd46225f6ae722c025f868494f5
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-06-15 18:37:08 +0900

    add/update: Reduxを通して、ステップデータをテーブルに表示

--- src/components/SkillTables/SkillTables.tsx
+++ src/components/SkillTables/SkillTables.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect, useCallback } from 'react'
+import { useDispatch , useSelector } from 'react-redux'
+import { fetchStepsData , StepsState } from '@/slice/stepSlice'
+import { AppDispatch } from '@/store/store'
 import { Step } from '@/@types/types'
 import { SkillTable } from '@/components'
-import { StepsState } from '@/slice/stepSlice'
-import { useSelector } from 'react-redux'
 import { Box, Grid } from '@mui/material'
 
 const SkillTables: React.FC = () => {
+  // src/store/store.ts の counter を指している。
   const steps = useSelector((state: { steps: StepsState }) => state.steps.value)
+  const dispatch: AppDispatch = useDispatch()
+  const fetchSteps = useCallback(() => {
+    dispatch(fetchStepsData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (steps.length === 0) return fetchSteps();
+    
+  }, [fetchSteps, steps]);
 
   const frontEndProps: Step[] = []
   const backEndProps: Step[] = []
@@ -21,24 +32,26 @@ const SkillTables: React.FC = () => {
   })
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container>
-        <Grid item sm={12} xs={12} md={12} lg={12} xl={6}>
-          <SkillTable
-            title="Front-End Goal Image"
-            link="https://github.com/Yuisei-Maruyama/MyPortfolio"
-            frontEndProps={frontEndProps}
-          />
+    steps.length ? (
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container>
+          <Grid item sm={12} xs={12} md={12} lg={12} xl={6}>
+            <SkillTable
+              title="Front-End Goal Image"
+              link="https://github.com/Yuisei-Maruyama/MyPortfolio"
+              frontEndProps={frontEndProps}
+            />
+          </Grid>
+          <Grid item sm={12} xs={12} md={12} lg={12} xl={6}>
+            <SkillTable
+              title="Back-End Goal Image"
+              link="https://github.com/Yuisei-Maruyama/MyPortfolio_Backend"
+              backEndProps={backEndProps}
+            />
+          </Grid>
         </Grid>
-        <Grid item sm={12} xs={12} md={12} lg={12} xl={6}>
-          <SkillTable
-            title="Back-End Goal Image"
-            link="https://github.com/Yuisei-Maruyama/MyPortfolio_Backend"
-            backEndProps={backEndProps}
-          />
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    ) : <></>
   )
 }
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,7 @@
-import React, { useEffect } from 'react'
-import type { GetStaticProps } from 'next'
+import React from 'react'
+// import type { GetStaticProps } from 'next'
 import styled from 'styled-components'
-import { useDispatch } from 'react-redux'
 import { Step } from '@/@types/types'
-import axios from 'axios'
-import { setSteps } from '@/slice/stepSlice'
 import { SkillTables } from '@/components'
 import variables from '../styles/variables.module.scss'
 
@@ -16,21 +13,14 @@ interface TextProps {
   color?: string
 }
 
-// SSG処理
-export const getStaticProps: GetStaticProps = async () => {
-  const req = await axios.get('http://localhost:3000/api/steps')
-  const steps: Step[] = req.data
-  return { props: { steps } }
-}
+// // SSG処理
+// export const getStaticProps: GetStaticProps = async () => {
+//   const req = await axios.get('http://localhost:3000/api/steps')
+//   const steps: Step[] = req.data
+//   return { props: { steps } }
+// }
 
 const Main: React.FC<Props> = ({ steps }) => {
-  // // src/store/store.ts の counter を指している。
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    if (!steps) return
-    dispatch(setSteps(steps))
-  }, [])
 
   return (
     <_Container>
