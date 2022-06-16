5c37c029e1159eb7ebc5484791109d06dca4d0f8
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-06-17 02:16:36 +0900

    add: 動的ルーティングの追加

---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,14 @@
+import Layout from "@/layout/Layout";
+
+const Post = () => {
+  return (
+    <Layout>
+      <p>
+      動的ルーティング
+      [id]の id はなんでもOK！！
+      </p>
+    </Layout>
+  );
+}
+
+export default Post;
