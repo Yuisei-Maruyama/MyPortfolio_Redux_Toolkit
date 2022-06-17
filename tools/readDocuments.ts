dcc76ae9ad251467cdaf66c375dcbf3e2cba50c4
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-06-17 14:28:48 +0900

    add: 指定したドキュメントのコンテンツを返すスクリプトの作成

---
+++ tools/readDocuments.ts
@@ -0,0 +1,24 @@
+import path from 'path'
+import { readdirSync, readFileSync } from 'fs'
+
+const documentsDir = path.join(process.cwd(), 'documents')
+const documentNames = readdirSync(documentsDir)
+
+// INFO: getStaticPath で使用するパス名を取得する
+// https://nextjs.org/docs/basic-features/data-fetching/get-static-paths
+export const getAllDocumentNames = () => {
+  return documentNames.map((fileName) => {
+    return {
+      params: {
+        name: fileName.replace(/\.md$/, ''),
+      },
+    }
+  })
+}
+
+// INFO: 対象ドキュメントの内容を取得
+export const getDocumentContent = (fileName: string) => {
+  const fullPath = path.join(documentsDir, `${fileName}.md`)
+  const documentContents = readFileSync(fullPath, 'utf-8')
+  return documentContents
+}
